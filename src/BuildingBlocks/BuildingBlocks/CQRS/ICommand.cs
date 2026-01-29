2c536d06cb5a5452dcb41b0baece7d1d69a77dfc
Jakub Wójtowicz <j.wojtowicz0105@gmail.com>
2026-01-29 20:57:09 +0100

    Create BuildingBlocks library project and add CQRS abstractions using MediatR
    BuildingBlocks class library will be referenced by every microservice.
    It will contain shared logic for microservices and packages.

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System.Windows.Input;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
