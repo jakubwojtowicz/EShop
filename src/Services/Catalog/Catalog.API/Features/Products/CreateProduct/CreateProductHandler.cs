938f359d625e3efc405bd5052d74ff55a9a29a05
Jakub Wójtowicz <j.wojtowicz0105@gmail.com>
2026-02-02 23:04:02 +0100

    Add GlobalUsings

--- src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductHandler.cs
@@ -1,26 +1,25 @@
 ﻿using BuildingBlocks.CQRS;
 using Catalog.API.Models;
 
-namespace Catalog.API.Features.Products.CreateProduct
+namespace Catalog.API.Features.Products.CreateProduct;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-        : ICommand<CreateProductResult>;
-    public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        var product = new Product
         {
-            var product = new Product
-            {
-                Name = command.Name,
-                Category = command.Category,
-                Description = command.Description,
-                ImageFile = command.ImageFile,
-                Price = command.Price,
-            };
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
 
-            return new CreateProductResult(Guid.NewGuid());
-        }
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
+
 
