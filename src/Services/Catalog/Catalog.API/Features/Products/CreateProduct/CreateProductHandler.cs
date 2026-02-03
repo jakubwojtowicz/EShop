bb6d8cc449e12532c67f55e6e7fcf227d159b99f
Jakub Wójtowicz <j.wojtowicz0105@gmail.com>
2026-02-03 21:00:38 +0100

    CatalogAPI: implemented CreateProduct CommandHandler using Marten

--- src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductHandler.cs
@@ -1,11 +1,9 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Features.Products.CreateProduct;
+﻿namespace Catalog.API.Features.Products.CreateProduct;
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler (IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -18,7 +16,10 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
             Price = command.Price,
         };
 
-        return new CreateProductResult(Guid.NewGuid());
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
     }
 }
 
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//Carter - minimal api endpoints
 builder.Services.AddCarter();
+
+//MediatR - CQRS pattern 
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+
+//Marten - ORM for document DB
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
