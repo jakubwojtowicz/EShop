7f8afaf63a2a79cf4c51456f883e6fefca65d520
Jakub Wójtowicz <j.wojtowicz0105@gmail.com>
2026-02-17 19:18:59 +0100

    Implement GetProductById feature

---
+++ src/Services/Catalog/Catalog.API/Features/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Features.Products.GetProductById;
+
+//public record GetProductByIdRequest();
+public record GetProductByIdResponse(Product Product);
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{productId}",
+            async (Guid productId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(productId));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product by ID")
+        .WithDescription("Get Product by ID");
+    }
+}
+
--- src/Services/Catalog/Catalog.API/Features/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Features/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,7 +1,7 @@
 ﻿namespace Catalog.API.Features.Products.GetProducts;
 
 public record GetProductsResponse(IEnumerable<Product> Products);
-public class GetProductsEndpoint : ICarterModule
+public class GetProductByIdEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
