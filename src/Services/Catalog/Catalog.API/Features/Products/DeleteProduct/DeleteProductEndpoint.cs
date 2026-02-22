e67a41585aef76fa478ef3394a88b5189b0ea4f5
Jakub Wójtowicz <j.wojtowicz0105@gmail.com>
2026-02-22 13:16:11 +0100

    Implement DeleteProduct feature

---
+++ src/Services/Catalog/Catalog.API/Features/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.API.Features.Products.DeleteProduct;
+
+//public record DeleteProductRequest(Guid Id);
+public record DeleteProductResponse(bool IsSuccess);
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{productId}",
+            async (Guid productId, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(productId));
+
+            var response = result.Adapt<DeleteProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteProduct")
+        .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete Product")
+        .WithDescription("Delete Product");
+    }
+}
+
